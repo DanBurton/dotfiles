e06bd476451994343baf69b06888b6a7a3455f65
Dan Burton <danburton.email@gmail.com>
2017-08-24 17:53:58 -0700

    bash_common.sh

---
+++ bash_common.sh
@@ -0,0 +1,63 @@
+## path modifications
+export PATH=/usr/local/sbin:$PATH
+export PATH=$HOME/.local/bin:$PATH
+
+if [ -e $HOME/.nix-profile/etc/profile.d/nix.sh ]; then . $HOME/.nix-profile/etc/profile.d/nix.sh; fi # added by Nix installer
+# modified by Dan to use $HOME
+# if [ -e /Users/danburton/.nix-profile/etc/profile.d/nix.sh ]; then . /Users/danburton/.nix-profile/etc/profile.d/nix.sh; fi # added by Nix installer
+
+## bash prompt
+DEFAULT='\[\e[m\]'
+GREEN='\[\e[0;32m\]'
+YELLOW='\[\e[0;33m\]'
+BLUE='\[\e[0;34m\]'
+PURPLE='\[\e[0;35m\]'
+CYAN='\[\e[0;36m\]'
+GRAY='\[\e[0;37m\]'
+
+# Black       0;30     Dark Gray     1;30
+# Blue        0;34     Light Blue    1;34
+# Green       0;32     Light Green   1;32
+# Cyan        0;36     Light Cyan    1;36
+# Red         0;31     Light Red     1;31
+# Purple      0;35     Light Purple  1;35
+# Brown       0;33     Yellow        1;33
+# Light Gray  0;37     White         1;37
+
+
+GITBRANCH='git rev-parse --abbrev-ref HEAD 2>/dev/null || echo "not a git repository"'
+GITDIRTY='[[ -n "$(git status -s 2> /dev/null)" ]] && echo "*"'
+export PS1="\n${CYAN}\w${PURPLE} [\$($GITBRANCH)\$($GITDIRTY)]\n${YELLOW}========================= ${GRAY}[\t] ${GREEN}\u ${YELLOW}=========================${DEFAULT}\n\$ "
+
+# colorized terminal output
+export CLICOLOR=1
+export LSCOLORS=gxBxhxDxfxhxhxhxhxcxcx
+
+# Bash completions
+eval "$(stack --bash-completion-script "$which stack")"
+if [ -f `brew --prefix`/etc/bash_completion ]; then
+    . `brew --prefix`/etc/bash_completion
+fi
+
+alias reload='source $HOME/.bash_profile'
+alias ls='ls -GF'
+
+alias scheck='stack update && stack --resolver ghc-8.2.1 exec stackage-curator check'
+
+uuidlower() {
+    ID="$(uuidgen | tr '[:upper:]' '[:lower:]')"
+    echo $ID | tr -d '\n' | pbcopy
+    echo $ID
+}
+
+docker-recompose() {
+  docker-compose down && docker-compose up -d
+}
+
+# Requires $GITHUB_USER
+gclone() {  
+  LOC=$(basename $(pwd))
+  git clone git@github.com:$LOC/$1.git
+  cd $1
+  git remote add forked-origin git@github.com:$GITHUB_USER/$1.git
+}
